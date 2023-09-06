6e2202aca3513a97488e22f5cca6cd333cb12a31
kochnevasf <68939762+kochnevasf@users.noreply.github.com>
2023-09-06 20:28:53 +0400

    Update conftest.py
    исправления

--- conftest.py
+++ conftest.py
@@ -4,12 +4,12 @@ import os
 import pytest
 
 WORK_DIR = os.path.dirname(os.path.abspath(__file__))
-RESOURCES_DIR = os.path.join(WORK_DIR, 'resources')
+RESOURCE_PATH = os.path.join(WORK_DIR, 'resources')
 
 
 @pytest.fixture(scope='function')
 def new_csv_file():
-    NEW_CSV_PATH = os.path.join(RESOURCES_DIR, 'new_csv.csv')
+    NEW_CSV_PATH = os.path.join(RESOURCE_PATH, 'new_csv.csv')
     with open(NEW_CSV_PATH, 'a') as csv_file:
         csvwriter = csv.writer(csv_file, delimiter=';')
         csvwriter.writerow(['Bonny', 'Born', 'Peter'])
